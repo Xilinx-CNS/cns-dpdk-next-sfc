DPDK/dpdk

commit 47c6782344b7cb606b8a6700747438504ddb2ddf

net/i40e: fix tunnel filter
Creating IPv4 flow and IPv6 flow will cause conflict error.
Root cause is there's no IP info included in tunnel filter
input.

Fixes: 425c3325f0b0 ("net/i40e: store tunnel filter")
Fixes: d416530e6358 ("net/i40e: parse tunnel filter")

Signed-off-by: Beilei Xing <beilei.xing@intel.com>

drivers/net/i40e/i40e_flow.c
@@ -1307,16 +1307,40 @@ i40e_flow_parse_vxlan_pattern(const struct rte_flow_item *pattern,
 			}
 			break;
 		case RTE_FLOW_ITEM_TYPE_IPV4:
+			filter->ip_type = RTE_TUNNEL_IPTYPE_IPV4;
+			/* IPv4 is used to describe protocol,
+			 * spec amd mask should be NULL.
+			 */
+			if (item->spec || item->mask) {
+				rte_flow_error_set(error, EINVAL,
+						   RTE_FLOW_ERROR_TYPE_ITEM,
+						   item,
+						   "Invalid IPv4 item");
+				return -rte_errno;
+			}
+			break;
 		case RTE_FLOW_ITEM_TYPE_IPV6:
+			filter->ip_type = RTE_TUNNEL_IPTYPE_IPV6;
+			/* IPv6 is used to describe protocol,
+			 * spec amd mask should be NULL.
+			 */
+			if (item->spec || item->mask) {
+				rte_flow_error_set(error, EINVAL,
+						   RTE_FLOW_ERROR_TYPE_ITEM,
+						   item,
+						   "Invalid IPv6 item");
+				return -rte_errno;
+			}
+			break;
 		case RTE_FLOW_ITEM_TYPE_UDP:
-			/* IPv4/IPv6/UDP are used to describe protocol,
+			/* UDP is used to describe protocol,
 			 * spec amd mask should be NULL.
 			 */
 			if (item->spec || item->mask) {
 				rte_flow_error_set(error, EINVAL,
 					   RTE_FLOW_ERROR_TYPE_ITEM,
 					   item,
-					   "Invalid IPv4 item");
+					   "Invalid UDP item");
 				return -rte_errno;
 			}
 			break;

drivers/net/i40e/i40e_ethdev.c
@@ -6705,6 +6705,12 @@ i40e_tunnel_filter_convert(struct i40e_aqc_add_remove_cloud_filters_element_data
 	ether_addr_copy((struct ether_addr *)&cld_filter->inner_mac,
 			(struct ether_addr *)&tunnel_filter->input.inner_mac);
 	tunnel_filter->input.inner_vlan = cld_filter->inner_vlan;
+	if ((rte_le_to_cpu_16(cld_filter->flags) &
+	     I40E_AQC_ADD_CLOUD_FLAGS_IPV6) ==
+	    I40E_AQC_ADD_CLOUD_FLAGS_IPV6)
+		tunnel_filter->input.ip_type = I40E_TUNNEL_IPTYPE_IPV6;
+	else
+		tunnel_filter->input.ip_type = I40E_TUNNEL_IPTYPE_IPV4;
 	tunnel_filter->input.flags = cld_filter->flags;
 	tunnel_filter->input.tenant_id = cld_filter->tenant_id;
 	tunnel_filter->queue = cld_filter->queue_number;

drivers/net/i40e/i40e_ethdev.h
@@ -499,11 +499,17 @@ struct i40e_ethertype_rule {
 /* Tunnel filter number HW supports */
 #define I40E_MAX_TUNNEL_FILTER_NUM 400
 
+enum i40e_tunnel_iptype {
+	I40E_TUNNEL_IPTYPE_IPV4,
+	I40E_TUNNEL_IPTYPE_IPV6,
+};
+
 /* Tunnel filter struct */
 struct i40e_tunnel_filter_input {
 	uint8_t outer_mac[6];    /* Outer mac address to match */
 	uint8_t inner_mac[6];    /* Inner mac address to match */
 	uint16_t inner_vlan;     /* Inner vlan address to match */
+	enum i40e_tunnel_iptype ip_type;
 	uint16_t flags;          /* Filter type flag */
 	uint32_t tenant_id;      /* Tenant id to match */
 };

Xilinx-CNS/cns-dpdk-next-sfc

commit 0849925edfaabd2f4ea2ad459638d9ee27a45516

net/i40e: fix a typo in flow
This patch fixes a trivial typo in i40e_flow.c.

Fixes: 47c6782344b7 ("net/i40e: fix tunnel filter")
Fixes: d416530e6358 ("net/i40e: parse tunnel filter")
Cc: stable@dpdk.org

Signed-off-by: Rami Rosen <rami.rosen@intel.com>
Acked-by: John McNamara <john.mcnamara@intel.com>

drivers/net/i40e/i40e_flow.c
@@ -1309,7 +1309,7 @@ i40e_flow_parse_vxlan_pattern(const struct rte_flow_item *pattern,
 		case RTE_FLOW_ITEM_TYPE_IPV4:
 			filter->ip_type = RTE_TUNNEL_IPTYPE_IPV4;
 			/* IPv4 is used to describe protocol,
-			 * spec amd mask should be NULL.
+			 * spec and mask should be NULL.
 			 */
 			if (item->spec || item->mask) {
 				rte_flow_error_set(error, EINVAL,
@@ -1322,7 +1322,7 @@ i40e_flow_parse_vxlan_pattern(const struct rte_flow_item *pattern,
 		case RTE_FLOW_ITEM_TYPE_IPV6:
 			filter->ip_type = RTE_TUNNEL_IPTYPE_IPV6;
 			/* IPv6 is used to describe protocol,
-			 * spec amd mask should be NULL.
+			 * spec and mask should be NULL.
 			 */
 			if (item->spec || item->mask) {
 				rte_flow_error_set(error, EINVAL,
@@ -1334,7 +1334,7 @@ i40e_flow_parse_vxlan_pattern(const struct rte_flow_item *pattern,
 			break;
 		case RTE_FLOW_ITEM_TYPE_UDP:
 			/* UDP is used to describe protocol,
-			 * spec amd mask should be NULL.
+			 * spec and mask should be NULL.
 			 */
 			if (item->spec || item->mask) {
 				rte_flow_error_set(error, EINVAL,