ba575bb04175304c90215f7e8a042d95701850b0
Andrew Rybchenko <arybchenko@solarflare.com>
2018-10-19 12:02:09 +0100

    net/sfc/base: add Riverhead NIC family
    Riverhead is the first NIC family of the EF100 architecture.
    
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/net/sfc/base/efx.h
+++ drivers/net/sfc/base/efx.h
@@ -42,6 +42,7 @@ typedef enum efx_family_e {
 	EFX_FAMILY_HUNTINGTON,
 	EFX_FAMILY_MEDFORD,
 	EFX_FAMILY_MEDFORD2,
+	EFX_FAMILY_RIVERHEAD,
 	EFX_FAMILY_NTYPES
 } efx_family_t;
 
@@ -54,6 +55,7 @@ efx_family(
 
 
 #define	EFX_PCI_VENID_SFC			0x1924
+#define	EFX_PCI_VENID_XILINX			0x10EE
 
 #define	EFX_PCI_DEVID_FALCON			0x0710	/* SFC4000 */
 
@@ -76,6 +78,8 @@ efx_family(
 #define	EFX_PCI_DEVID_MEDFORD2			0x0B03	/* SFC9250 PF */
 #define	EFX_PCI_DEVID_MEDFORD2_VF		0x1B03	/* SFC9250 VF */
 
+#define	EFX_PCI_DEVID_RIVERHEAD			0x0100
+#define	EFX_PCI_DEVID_RIVERHEAD_VF		0x1100
 
 #define	EFX_MEM_BAR_SIENA			2
 
@@ -86,6 +90,8 @@ efx_family(
 #define	EFX_MEM_BAR_MEDFORD_VF			0
 
 #define	EFX_MEM_BAR_MEDFORD2			0
+
+#define	EFX_MEM_BAR_RIVERHEAD			2
 
 
 /* Error codes */
