a07386a199250bdc2105aa553c34d578381d16d4
Ivan Malov <ivan.malov@oktetlabs.ru>
2019-12-20 16:33:56 +0300

    common/sfc_efx/base: indicate support for MAE
    Riverhead boards maintain support for MAE, a low-level Match-Action Engine.
    The feature is documented in SF-122526-TC.
    
    The new field will help client drivers to test
    NIC support for MAE status.
    
    Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
    Reviewed-by: Andy Moreton <amoreton@xilinx.com>

--- drivers/common/sfc_efx/base/efx_check.h
+++ drivers/common/sfc_efx/base/efx_check.h
@@ -410,4 +410,10 @@
 # endif
 #endif /* EFSYS_OPT_VIRTIO */
 
+#if EFSYS_OPT_MAE
+# if !EFSYS_OPT_RIVERHEAD
+#  error "MAE requires RIVERHEAD"
+# endif
+#endif /* EFSYS_OPT_MAE */
+
 #endif /* _SYS_EFX_CHECK_H */
