0582158868c2943b46b94f3e60f4844e8418d1dc
Igor Romanov <igor.romanov@oktetlabs.ru>
2020-08-24 12:00:13 +0100

    COMMANDS HERE DEBUG: hardcode MAC for VF since EVB is not available to get it
    export TAP_HOST="superx9b"
    export TAP_IF="tap_${USER}"
    export TAP_TCP_PORT=53417
    export CMOD_HOST=anvil
    export CMOD_SSH=9037
    export CMOD_RCF=9038
    
    The branch was tested using the following commands:
    
    1) Prepare fw versions:
    smartnic_fw: HEAD (28e2fcea98737da55934bebdaa421f692757f203)
    smartnic_cmodel: HEAD (8e7d51ac2efcd0f4a5dd0be0ab04733223761644)
    smartnic-snapper: user/osadakov/CT-8489 (95b8f6f6483876056b77552fe5f37de44d597bdd)
    or
    smartnic-snapper: master
    
    2) Run cmodel:
     /home/romigor/src/smartnic-snapper/src/tools/cosim/esnap \
    --identity romigor \
    --chip rhead \
    --runtime batfish_mae_slice/src/apps/riverhead/cmc \
    --rhead-mae \
    --elfboot \
    --dutcfg just2pfwith2vf \
    --mac-25G \
    --mcfw-flash /projects/riverhead/Release/flash_images/p7b/cmod_dev/ef100_vnet_vblk.mcs \
    --mac-loopback \
    --q35 \
    --qemu-version stable \
    --enable-kvm \
    --vio-cfgcaps \
    --no-xaccel \
    --pciid 10ee:0100,10ee:0100,1bf4:1000/1bf4:0001,1bf4:1001/1bf4:0002 \
    --mac-addr 00:0e:ad:be:ef:03/2 \
    --virtmem 2048 \
    --qemu-smp 4 \
    --ro-dir /home/romigor/src/dpdk-next-net \
    --kernel-append default_hugepagesz=2M \
    --kernel-append hugepagesz=2M \
    --kernel-append hugepages=512 \
    --init \
    --qemu-hostnet-sshd "$CMOD_SSH" \
    --fnet0 "$TAP_HOST":"$TAP_TCP_PORT"
    
    3) Setup VFs inside cmodel:
    /v5/build/falcon_b0_driver/driver/linux_virt2phys/load.sh
    echo AUTO > /sys/bus/pci/devices/0000:01:00.0/sriov
    echo Y > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
    /ro/usertools/dpdk-devbind.py --bind=vfio-pci 0000:01:00.0 0000:01:00.4 0000:01:00.5
    /root/bin/mknod_vfio
    
    4) Test port representor with testpmd:
    4.1) Rx:
    a) /ro/build/app/dpdk-testpmd --log-level=pmd.net.sfc.*,8 -w 0000:01:00.0,switch_mode=switchdev,representor=[0,1] --file-prefix=PF -l 0,1,2,3 -S 3 -- --forward-mode=rxonly --burst=1 --rxd=256 --txd=256 -i
    b) /ro/build/app/dpdk-testpmd --log-level=pmd.net.sfc.*,8 -w 0000:01:00.4,switch_mode=switchdev -w 0000:01:00.5,switch_mode=switchdev --file-prefix=VF -l 0,1,2,3 -S 3 -- --forward-mode=txonly --burst=1 --rxd=256 --txd=16 --eth-peer=0,00:01:02:03:04:05 -i
    
    4.2) Tx:
    a) /ro/build/app/dpdk-testpmd --log-level=pmd.net.sfc.*,8 -w 0000:01:00.0,representor=[0,1],switch_mode=switchdev --file-prefix=PF -l 0,1,2,3 -S 3 -- --forward-mode=txonly --burst=1 --rxd=256 --txd=256 -i --eth-peer=1,02:42:00:00:00:04
    b) /ro/build/app/dpdk-testpmd --log-level=pmd.net.sfc.*,8 -w 0000:01:00.4,switch_mode=switchdev -w 0000:01:00.5,switch_mode=switchdev --file-prefix=VF -l 0,1,2,3 -S 3 -- --forward-mode=rxonly --burst=1 --rxd=256 --txd=16 -i
    
    5) Test using dpdk-pmd-ts:
    ../src/dpdk-ts/scripts/run.sh -q --cfg="rheadington:anvil:$CMOD_SSH:$CMOD_CF:"$TAP_HOST":$TAP_IF" --tester-run=dpdk-pmd-ts/representors/hotplug

--- drivers/common/sfc_efx/base/ef10_nic.c
+++ drivers/common/sfc_efx/base/ef10_nic.c
@@ -1837,7 +1837,7 @@ efx_mcdi_nic_board_cfg(
 	uint32_t mask;
 	uint32_t pf;
 	uint32_t vf;
-	uint8_t mac_addr[6] = { 0 };
+	uint8_t mac_addr[6] = { 0x02, 0x42, 0x00, 0x00, 0x00, encp->enc_vf };
 	efx_rc_t rc;
 
 	/* Get the (zero-based) MCDI port number */
@@ -1889,7 +1889,8 @@ efx_mcdi_nic_board_cfg(
 		}
 #endif /* EFSYS_OPT_ALLOW_UNCONFIGURED_NIC */
 	} else {
-		rc = efx_mcdi_get_mac_address_vf(enp, mac_addr);
+		rc = 0;
+		/* rc = efx_mcdi_get_mac_address_vf(enp, mac_addr); */
 	}
 	if (rc != 0)
 		goto fail5;
