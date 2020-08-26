9c194a2563e91a313042570287083b844d49f4b7
Igor Romanov <igor.romanov@oktetlabs.ru>
2020-08-26 12:20:18 +0100

    net/sfc: insert switchdev mode MAE rules
    By default, the firmware is in EVB mode, but insertion of the first MAE
    rule resets it to switchdev mode automatically and removes all automatic
    MAE rules added by EVB support. On initialisation, insert MAE rules that
    forward traffic between PHY and PF.
    
    Add an API for creation and insertion of driver-internal MAE
    rules(flows).
    
    Signed-off-by: Igor Romanov <igor.romanov@oktetlabs.ru>
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Reviewed-by: Andy Moreton <amoreton@xilinx.com>
    Reviewed-by: Ivan Malov <ivan.malov@oktetlabs.ru>

--- drivers/net/sfc/sfc.c
+++ drivers/net/sfc/sfc.c
@@ -885,6 +885,10 @@ sfc_attach(struct sfc_adapter *sa)
 	if (rc != 0)
 		goto fail_mae_attach;
 
+	rc = sfc_mae_switchdev_init(sa);
+	if (rc != 0)
+		goto fail_mae_switchdev_init;
+
 	sfc_log_init(sa, "fini nic");
 	efx_nic_fini(enp);
 
@@ -906,6 +910,9 @@ sfc_attach(struct sfc_adapter *sa)
 
 fail_sriov_vswitch_create:
 	sfc_flow_fini(sa);
+	sfc_mae_switchdev_fini(sa);
+
+fail_mae_switchdev_init:
 	sfc_mae_detach(sa);
 
 fail_mae_attach:
@@ -952,6 +959,7 @@ sfc_detach(struct sfc_adapter *sa)
 
 	sfc_flow_fini(sa);
 
+	sfc_mae_switchdev_fini(sa);
 	sfc_mae_detach(sa);
 	sfc_mae_counter_rxq_detach(sa);
 	sfc_filter_detach(sa);
