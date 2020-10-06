Xilinx-CNS/cns-dpdk-next-sfc

commit af3efe6fb3ccaffd3214308772f96fe46a3ca366

net/sfc: support flow action PORT ID in transfer rules
The action handler will use MAE action DELIVER with MPORT
of the PCIe function associated with a given DPDK port ID.
The DPDK port must not relate to a different physical device.

Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>
Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
Reviewed-by: Andy Moreton <amoreton@xilinx.com>

drivers/net/sfc/sfc_mae.c
@@ -118,18 +118,40 @@ sfc_mae_find_switch_port_by_entity(const struct sfc_mae_switch_domain *domain,
 	return NULL;
 }
 
+/* This function expects to be called only when the lock is held */
+static struct sfc_mae_switch_port *
+sfc_mae_find_switch_port_by_ethdev(const struct sfc_mae_switch_domain *domain,
+				   uint16_t port_id)
+{
+	struct sfc_mae_switch_port *port;
+
+	SFC_ASSERT(rte_spinlock_is_locked(&sfc_mae_switch.lock));
+
+	if (port_id == RTE_MAX_ETHPORTS)
+		return NULL;
+
+	TAILQ_FOREACH(port, &domain->ports, switch_domain_ports) {
+		if (port->ethdev_port_id == port_id)
+			return port;
+	}
+
+	return NULL;
+}
+
 struct sfc_mae_switch_port_request {
 	enum sfc_mae_switch_port_type		type;
 	const efx_mport_sel_t			*entity_mportp;
+	const efx_mport_sel_t			*ethdev_mportp;
 };
 
 /* This function expects to be called only when the lock is held */
 static int
-sfc_mae_assign_switch_port(__rte_unused struct sfc_adapter *sa,
+sfc_mae_assign_switch_port(struct sfc_adapter *sa,
 			   struct sfc_mae_switch_domain *domain,
 			   const struct sfc_mae_switch_port_request *req,
 			   struct sfc_mae_switch_port **portp)
 {
+	struct sfc_adapter_shared * const sas = sfc_sa2shared(sa);
 	struct sfc_mae_switch_port *port;
 
 	SFC_ASSERT(rte_spinlock_is_locked(&sfc_mae_switch.lock));
@@ -151,6 +173,8 @@ sfc_mae_assign_switch_port(__rte_unused struct sfc_adapter *sa,
 	TAILQ_INSERT_TAIL(&domain->ports, port, switch_domain_ports);
 
 done:
+	port->ethdev_mport.sel = req->ethdev_mportp->sel;
+	port->ethdev_port_id = sas->port_id;
 	*portp = port;
 
 	return 0;
@@ -228,6 +252,11 @@ sfc_mae_attach(struct sfc_adapter *sa)
 	sfc_log_init(sa, "assign RTE switch parameters");
 	switch_port_request.type = SFC_MAE_SWITCH_PORT_INDEPENDENT;
 	switch_port_request.entity_mportp = &entity_mport;
+	/*
+	 * As of now, the driver does not support representors, so
+	 * RTE ethdev MPORT simply matches that of the entity.
+	 */
+	switch_port_request.ethdev_mportp = &entity_mport;
 	rc = sfc_mae_assign_switch_parameters(sa, &switch_port_request,
 					      &switch_domain, &switch_port);
 	if (rc != 0)
@@ -270,6 +299,9 @@ sfc_mae_detach(struct sfc_adapter *sa)
 
 	SFC_ASSERT(mae->action_rc_cache.class_handle.h == EFX_MAE_HANDLE_NULL);
 	SFC_ASSERT(mae->action_rc_cache.match_spec == NULL);
+
+	/* Invalidate RTE ethdev port ID in the switch port entry */
+	mae->switch_port->ethdev_port_id = RTE_MAX_ETHPORTS;
 
 	efx_mae_fini(sa->nic);
 
@@ -942,6 +974,35 @@ sfc_mae_rule_parse_action_pf_vf(struct sfc_adapter *sa,
 }
 
 static int
+sfc_mae_rule_parse_action_port_id(struct sfc_adapter *sa,
+				  const struct rte_flow_action_port_id *conf,
+				  efx_mae_actions_t *spec)
+{
+	const struct sfc_mae_switch_port *switch_port;
+	struct sfc_mae *mae = &sa->mae;
+	efx_mport_sel_t mport;
+
+	rte_spinlock_lock(&sfc_mae_switch.lock);
+
+	if (conf->original != 0) {
+		mport.sel = mae->switch_port->ethdev_mport.sel;
+	} else {
+		switch_port = sfc_mae_find_switch_port_by_ethdev(
+						mae->switch_domain, conf->id);
+		if (switch_port == NULL) {
+			rte_spinlock_unlock(&sfc_mae_switch.lock);
+			return EINVAL;
+		}
+
+		mport.sel = switch_port->ethdev_mport.sel;
+	}
+
+	rte_spinlock_unlock(&sfc_mae_switch.lock);
+
+	return efx_mae_action_set_populate_deliver(spec, &mport);
+}
+
+static int
 sfc_mae_rule_parse_action(struct sfc_adapter *sa,
 			  const struct rte_flow_action *action,
 			  struct sfc_mae_actions_bundle *bundle,
@@ -995,6 +1056,11 @@ sfc_mae_rule_parse_action(struct sfc_adapter *sa,
 		SFC_BUILD_SET_OVERFLOW(RTE_FLOW_ACTION_TYPE_VF,
 				       bundle->actions_mask);
 		rc = sfc_mae_rule_parse_action_pf_vf(sa, action->conf, spec);
+		break;
+	case RTE_FLOW_ACTION_TYPE_PORT_ID:
+		SFC_BUILD_SET_OVERFLOW(RTE_FLOW_ACTION_TYPE_PORT_ID,
+				       bundle->actions_mask);
+		rc = sfc_mae_rule_parse_action_port_id(sa, action->conf, spec);
 		break;
 	case RTE_FLOW_ACTION_TYPE_DROP:
 		SFC_BUILD_SET_OVERFLOW(RTE_FLOW_ACTION_TYPE_DROP,

drivers/net/sfc/sfc_mae.h
@@ -52,9 +52,21 @@ enum sfc_mae_switch_port_type {
  * This mapping comprises a port type to ensure that RTE switch port ID
  * of a represented entity and that of its representor are different in
  * the case when the entity gets plugged into DPDK and not into a guest.
+ *
+ * Entry data also comprises RTE ethdev's own MPORT. This value
+ * coincides with the entity MPORT in the case of independent ports.
+ * In the case of representors, this ID is not a selector and refers
+ * to an allocatable object (that is, it's likely to change on RTE
+ * ethdev replug). Flow API backend must use this value rather
+ * than entity_mport to support flow rule action PORT_ID.
  */
 struct sfc_mae_switch_port {
 	TAILQ_ENTRY(sfc_mae_switch_port)	switch_domain_ports;
+
+	/** RTE ethdev MPORT */
+	efx_mport_sel_t				ethdev_mport;
+	/** RTE ethdev port ID */
+	uint16_t				ethdev_port_id;
 
 	/** Entity (PCIe function) MPORT selector */
 	efx_mport_sel_t				entity_mport;

commit cecdd8ab6fd917a8b7ff8500ef221d8e419f6152

net/sfc: remove unused parameters from switch infrastructure
ethdev M-port is not used any more.

Make unnecessary to specify it on request.

Fixes: af3efe6fb3cc ("net/sfc: support flow action PORT ID in transfer rules")

Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

drivers/net/sfc/sfc_mae.c
@@ -76,9 +76,8 @@ sfc_mae_attach(struct sfc_adapter *sa)
 	switch_port_request.entity_mportp = &entity_mport;
 	/*
 	 * As of now, the driver does not support representors, so
-	 * RTE ethdev MPORT simply matches that of the entity.
+	 * RTE ethdev port ID is port ID of this adapter.
 	 */
-	switch_port_request.ethdev_mportp = &entity_mport;
 	switch_port_request.ethdev_port_id = sas->port_id;
 	rc = sfc_mae_assign_switch_port(mae->switch_domain_id,
 					&switch_port_request,

drivers/net/sfc/sfc_switch.c
@@ -52,8 +52,6 @@
 struct sfc_mae_switch_port {
 	TAILQ_ENTRY(sfc_mae_switch_port)	switch_domain_ports;
 
-	/** RTE ethdev MPORT */
-	efx_mport_sel_t				ethdev_mport;
 	/** RTE ethdev port ID */
 	uint16_t				ethdev_port_id;
 
@@ -275,7 +273,6 @@ sfc_mae_assign_switch_port(uint16_t switch_domain_id,
 	TAILQ_INSERT_TAIL(&domain->ports, port, switch_domain_ports);
 
 done:
-	port->ethdev_mport = *req->ethdev_mportp;
 	port->ethdev_port_id = req->ethdev_port_id;
 
 	*switch_port_id = port->id;

drivers/net/sfc/sfc_switch.h
@@ -32,7 +32,6 @@ enum sfc_mae_switch_port_type {
 struct sfc_mae_switch_port_request {
 	enum sfc_mae_switch_port_type		type;
 	const efx_mport_sel_t			*entity_mportp;
-	const efx_mport_sel_t			*ethdev_mportp;
 	uint16_t				ethdev_port_id;
 };
 