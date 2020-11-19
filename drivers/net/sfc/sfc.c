426a06a49d01186e775e8f36cee7172966b242c6
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2020-11-19 09:34:17 +0000

    net/sfc: HACK: unbind representors support from number of VFs

--- drivers/net/sfc/sfc.c
+++ drivers/net/sfc/sfc.c
@@ -36,8 +36,10 @@ sfc_repr_supported(const struct sfc_adapter *sa)
 	if (!sa->switchdev)
 		return false;
 
+#if 0
 	if (sa->sriov.num_vfs == 0)
 		return false;
+#endif
 
 	/*
 	 * Representor proxy should use service lcore on PF's socket
