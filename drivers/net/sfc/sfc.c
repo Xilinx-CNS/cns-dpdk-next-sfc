b43ec45007becfd83a85d5c7c37d1ef0d70894a3
Igor Romanov <igor.romanov@oktetlabs.ru>
2020-12-29 09:50:40 +0300

    net/sfc: add representor proxy port API
    The API is required to create and destroy representor proxy
    port assigned to representor.
    
    Signed-off-by: Igor Romanov <igor.romanov@oktetlabs.ru>
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Reviewed-by: Andy Moreton <amoreton@xilinx.com>
    Reviewed-by: Ivan Malov <ivan.malov@oktetlabs.ru>

--- drivers/net/sfc/sfc.c
+++ drivers/net/sfc/sfc.c
@@ -1048,6 +1048,18 @@ fail_nic_reset:
 }
 
 void
+sfc_pre_detach(struct sfc_adapter *sa)
+{
+	sfc_log_init(sa, "entry");
+
+	SFC_ASSERT(!sfc_adapter_is_locked(sa));
+
+	sfc_repr_proxy_pre_detach(sa);
+
+	sfc_log_init(sa, "done");
+}
+
+void
 sfc_detach(struct sfc_adapter *sa)
 {
 	sfc_log_init(sa, "entry");
