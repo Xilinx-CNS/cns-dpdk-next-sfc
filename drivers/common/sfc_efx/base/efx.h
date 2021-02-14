292ca84fa8433afdf522706be9c497943083d76c
Ivan Malov <ivan.malov@oktetlabs.ru>
2021-02-14 23:15:51 +0300

    common/sfc_efx/base: support encap. header provisioning
    Let the client allocate / free encap. headers.
    
    Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>

--- drivers/common/sfc_efx/base/efx.h
+++ drivers/common/sfc_efx/base/efx.h
@@ -4092,6 +4092,7 @@ typedef struct efx_mae_limits_s {
 	uint32_t			eml_max_n_action_prios;
 	uint32_t			eml_max_n_outer_prios;
 	uint32_t			eml_encap_types_supported;
+	uint32_t			eml_encap_header_size_limit;
 	uint32_t			eml_max_n_counters;
 } efx_mae_limits_t;
 
@@ -4405,6 +4406,26 @@ extern	__checkReturn			efx_rc_t
 efx_mae_action_set_fill_in_counter_id(
 	__in				efx_mae_actions_t *spec,
 	__in				const efx_counter_t *counter_idp);
+
+/* Encap. header ID */
+typedef struct efx_mae_eh_id_s {
+	uint32_t id;
+} efx_mae_eh_id_t;
+
+LIBEFX_API
+extern	__checkReturn			efx_rc_t
+efx_mae_encap_header_alloc(
+	__in				efx_nic_t *enp,
+	__in				efx_tunnel_protocol_t encap_type,
+	__in_bcount(header_size)	uint8_t *header_data,
+	__in				size_t header_size,
+	__out				efx_mae_eh_id_t *eh_idp);
+
+LIBEFX_API
+extern	__checkReturn			efx_rc_t
+efx_mae_encap_header_free(
+	__in				efx_nic_t *enp,
+	__in				const efx_mae_eh_id_t *eh_idp);
 
 /* Action set ID */
 typedef struct efx_mae_aset_id_s {
