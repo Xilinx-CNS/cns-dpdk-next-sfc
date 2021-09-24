04ffd497a0a3228782c408d04cdd28764a439fee
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2021-09-24 12:57:01 +0300

    net/sfc: support represented port flow item
    Add support for RTE_FLOW_ITEM_TYPE_REPRESENTED_PORT to match on
    traffic coming from external world (either network port or
    represented function).
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- drivers/net/sfc/sfc_switch.h
+++ drivers/net/sfc/sfc_switch.h
@@ -106,6 +106,10 @@ int sfc_mae_switch_port_by_ethdev(uint16_t switch_domain_id,
 				  uint16_t ethdev_port_id,
 				  efx_mport_sel_t *mport_sel);
 
+int sfc_mae_switch_port_entity_by_ethdev(uint16_t switch_domain_id,
+					 uint16_t ethdev_port_id,
+					 efx_mport_sel_t *entity_mport_sel);
+
 int sfc_mae_switch_port_id_by_entity(uint16_t switch_domain_id,
 				     const efx_mport_sel_t *entity_mportp,
 				     enum sfc_mae_switch_port_type type,
